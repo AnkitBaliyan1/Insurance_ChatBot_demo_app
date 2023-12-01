12f3bc7d5dd966d6a5681d08b2665d879e352b03
Ankit Baliyan <a.baliyan008@gmail.com>
2023-12-01 14:29:37 +0530

    update

--- data_base_model.py
+++ data_base_model.py
@@ -2,84 +2,12 @@ import streamlit as st
 #import os
 from openai._client import OpenAI
 import time
+import pandas as pd
 
 API_KEY = st.secrets['OPENAI_API']
 
-def database_app():
-    client = OpenAI(api_key = API_KEY)
-
-    trained_files = ['9.3 INSURANCE LAW AND PRACTICE_CS.pdf',
-        'Guidebook on Health Insurance.pdf',
-        'Insurance_Basics information ebook.pdf',
-        'Principles of Insurance_LOMA.pdf',
-        'ic 14 regulation of ins business for Licentiate Exam of III.pdf',
-        'Health Insurance: Are We Covered? - Sorin Investments.pdf',
-        'Q&A_ insurance & reinsurance regulation in India - Lexology.pdf']
-    
-    with st.container():
-        st.write("Files the model is trained for: ")
-        for i, file in enumerate(trained_files):
-            st.write(f"{i+1}. {file}")
-
-    # these files are already given to the assistent, with following file_id
-    file_ids = ['file-hDhdvqkJChYdkzttGFZ6S28b',
-    'file-7D9IReBqreOlI2nmAa89KXvy',
-    'file-NgBpE07Ve7WcdOJoVHNvI0rl',
-    'file-NYABDUzSMSrgBDyd8TYoCspj',
-    'file-rurOHDUhqHTq8LvL3geWlUKb',
-    'file-q4jVdVJfLvuYhIwnS5tS9mzE',
-    'file-KMYHVuEHwdd2NxiUsI6sUVlN']
-
-    assistant_id = 'asst_LMC3TpkbxsFtbVPcPH3UGoec'
-
-    # creating thread 
-    thread = client.beta.threads.create()
-
-
-    # user question
-    user_prompt = st.text_input("How can I help you?")
-    #"what is the Special Conditions and Exclusions in this policy? The response should be in simple language."
-
-
-    if st.button("Generate Response"):
-        if not user_prompt:
-            st.error("Please enter your question above.")
-        else:
-            with st.spinner():
-                # prompting user question to the assistant
-                ## note: here that the user prompt is added to the thread.
-                message = client.beta.threads.messages.create(
-                    thread_id=thread.id,
-                    role="user",
-                    content= user_prompt
-                )
-
-                # runs to converge thread id and assistant id
-                run = client.beta.threads.runs.create(
-                thread_id=thread.id,
-                assistant_id=assistant_id,
-                instructions="Response should end with I hope you liked the response. Let's try another one."
-                )
-
-                to_run=True
-                while to_run:
-                    # converge run id and thread id to overlap with assistant.
-                    run = client.beta.threads.runs.retrieve(
-                    thread_id=thread.id,
-                    run_id=run.id
-                    )
-                    time.sleep(1)
-                    if run.status == 'completed':
-                        to_run=False
-
-                # reterive messages from from assistant
-                messages = client.beta.threads.messages.list(
-                thread_id=thread.id
-                )
-
-                # creating it to view the response and prompt
-                for i in reversed(messages.data):
-                    st.write(i.role + " :")
-                    st.write(i.content[0].text.value)
-
+current_log_book = pd.DataFrame(columns=['user','time','question','answer'])
+log_book_path = "../logbook/logbook.csv"
+log_database = pd.read_csv(log_book_path)
+st.write(log_database)
 
--- temp.py
+++ temp.py
@@ -1,42 +1,78 @@
+def database_app():
+    client = OpenAI(api_key = API_KEY)
 
-# Main app
-def main_app():
-    # Display username with icon and smaller font at the top right
-    if st.sidebar.button('Logout'):
-        logout()
+    trained_files = ['9.3 INSURANCE LAW AND PRACTICE_CS.pdf',
+        'Guidebook on Health Insurance.pdf',
+        'Insurance_Basics information ebook.pdf',
+        'Principles of Insurance_LOMA.pdf',
+        'ic 14 regulation of ins business for Licentiate Exam of III.pdf',
+        'Health Insurance: Are We Covered? - Sorin Investments.pdf',
+        'Q&A_ insurance & reinsurance regulation in India - Lexology.pdf']
+    
+    with st.container():
+        st.write("Files the model is trained for: ")
+        for i, file in enumerate(trained_files):
+            st.write(f"{i+1}. {file}")
 
-    #st.markdown(f"<h2 style='text-align: right; color: gray;'>{st.session_state['username']}</h2>", unsafe_allow_html=True)
+    # these files are already given to the assistent, with following file_id
+    file_ids = ['file-hDhdvqkJChYdkzttGFZ6S28b',
+    'file-7D9IReBqreOlI2nmAa89KXvy',
+    'file-NgBpE07Ve7WcdOJoVHNvI0rl',
+    'file-NYABDUzSMSrgBDyd8TYoCspj',
+    'file-rurOHDUhqHTq8LvL3geWlUKb',
+    'file-q4jVdVJfLvuYhIwnS5tS9mzE',
+    'file-KMYHVuEHwdd2NxiUsI6sUVlN']
+
+    assistant_id = 'asst_LMC3TpkbxsFtbVPcPH3UGoec'
+
+    # creating thread 
+    thread = client.beta.threads.create()
 
 
-    st.markdown(f"""
-        <div style='text-align: right;'>
-            <span style='font-size: 16px; margin-right: 10px;'>{st.session_state['username']}</span>
-            <a href='javascript:void(0);' onclick='if (confirm("Do you want to logout?")) {{window.streamlitPythonData = {{logout: true}};}}'>
-                <img src='https://img.icons8.com/ios-filled/50/000000/user-male-circle.png' style='vertical-align: middle; width: 24px;'>
-            </a>
-        </div>
-        """, unsafe_allow_html=True)
+    # user question
+    user_prompt = st.text_input("How can I help you?")
+    #"what is the Special Conditions and Exclusions in this policy? The response should be in simple language."
 
-    with st.sidebar:
-        selected = option_menu("Main Menu", 
-            ["About", "ChatBot", "Build your onw ChatBot","Info", "Contact Us"],
-            icons=['house', 'cart', 'cart', 'info', 'envelope'],
-            menu_icon="cast", default_index=0)
 
-    if selected == 'About':
-        about.main()
-    elif selected == 'ChatBot':
-        st.write("database app now")
-        #data_base_model.database_app()
-    elif selected == "Build your onw ChatBot":
-        user_file_model.main()
-    elif selected == 'Info':
-        info_page()
-    elif selected == 'Contact Us':
-        contact_page()
+    if st.button("Generate Response"):
+        if not user_prompt:
+            st.error("Please enter your question above.")
+        else:
+            with st.spinner():
+                # prompting user question to the assistant
+                ## note: here that the user prompt is added to the thread.
+                message = client.beta.threads.messages.create(
+                    thread_id=thread.id,
+                    role="user",
+                    content= user_prompt
+                )
 
-# App start
-if 'logged_in' not in st.session_state or not st.session_state['logged_in']:
-    login_page()
-else:
-    main_app()
+                # runs to converge thread id and assistant id
+                run = client.beta.threads.runs.create(
+                thread_id=thread.id,
+                assistant_id=assistant_id,
+                instructions="Response should end with I hope you liked the response. Let's try another one."
+                )
+
+                to_run=True
+                while to_run:
+                    # converge run id and thread id to overlap with assistant.
+                    run = client.beta.threads.runs.retrieve(
+                    thread_id=thread.id,
+                    run_id=run.id
+                    )
+                    time.sleep(1)
+                    if run.status == 'completed':
+                        to_run=False
+
+                # reterive messages from from assistant
+                messages = client.beta.threads.messages.list(
+                thread_id=thread.id
+                )
+
+                # creating it to view the response and prompt
+                for i in reversed(messages.data):
+                    st.write(i.role + " :")
+                    st.write(i.content[0].text.value)
+
+
