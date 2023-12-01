67998780c49f1eb6a0229d278a19da8bb8db5930
Ankit Baliyan <a.baliyan008@gmail.com>
2023-12-01 14:31:42 +0530

    Update data_base_model.py

--- data_base_model.py
+++ data_base_model.py
@@ -7,8 +7,8 @@ import pandas as pd
 API_KEY = st.secrets['OPENAI_API']
 
 current_log_book = pd.DataFrame(columns=['user','time','question','answer'])
-log_book_path = "git_Insurance_ChatBot_demo_app/logbook/logbook.csv"
+#log_book_path = "git_Insurance_ChatBot_demo_app/logbook/logbook.csv"
 
-log_database = pd.read_csv(log_book_path)
-st.write(log_database)
+#log_database = pd.read_csv(log_book_path)
+st.write(current_log_book)
 
