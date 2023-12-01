11bc93d42ee3ec8aa5b2194d618ae0d6cd0b2415
Ankit Baliyan <a.baliyan008@gmail.com>
2023-12-01 14:38:32 +0530

    Update data_base_model.py

--- data_base_model.py
+++ data_base_model.py
@@ -7,15 +7,7 @@ import os
 
 API_KEY = st.secrets['OPENAI_API']
 
-current_log_book = pd.DataFrame(columns=['user','time','question','answer'])
-log_book_path = "/logbook/logbook.csv"
-
-log_database = pd.read_csv(log_book_path)
-
-with open(log_book_path,'rb') as file:
-    df = file.read()
 
 
 def database_app():
-    st.write(df)
-    
+    st.wirte("runing")
