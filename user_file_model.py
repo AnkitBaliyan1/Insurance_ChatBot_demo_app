98ba6b3ec1dd3459cb3a606ffcbada34b8679a16
Ankit Baliyan <a.baliyan008@gmail.com>
2023-12-02 21:35:05 +0530

    Update user_file_model.py

--- user_file_model.py
+++ user_file_model.py
@@ -1,23 +1,8 @@
 import streamlit as st
 
 
-# Function to validate login
-def validate_login(username, password):
-    user_credentials = st.secrets["users"].get(username)
-    if user_credentials and user_credentials["password"] == password:
-        return True
-    return False
 
 def main():
     st.subheader("Comming Soon...!")
 
-    # Login form
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
-
-    if st.button("Login"):
-        if validate_login(username, password):
-            st.success("Login successful!")
-            # Perform actions for logged-in user
-        else:
-            st.error("Login failed. Check your username and password.")
+    
